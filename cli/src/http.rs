07e070c5d4f25861bfdcfad754be4e028dc08236
nikstur <nikstur@outlook.com>
2022-03-19 18:52:07 +0100

    Implemented HTTP client code

--- cli/src/http.rs
+++ cli/src/http.rs
@@ -2,9 +2,13 @@ use std::fs;
 use std::path::Path;
 
 use anyhow::Result;
-use recesser_core::metadata::Metadata;
+use recesser_core::{metadata::Metadata, repository::NewRepository};
 use reqwest::blocking::{self, multipart, Response};
 pub use reqwest::StatusCode;
+
+const A: &str = "/artifacts";
+const R: &str = "/repositories";
+const U: &str = "/users";
 
 pub struct Client {
     addr: String,
@@ -19,7 +23,21 @@ impl Client {
         }
     }
 
-    pub fn upload(&self, handle: &str, metadata: Metadata, filepath: &Path) -> Result<Response> {
+    fn url(&self, path: &str) -> String {
+        format!("{addr}{path}", addr = self.addr)
+    }
+}
+
+pub trait ArtifactEndpoints {
+    fn upload_file(&self, handle: &str, metadata: Metadata, filepath: &Path) -> Result<Response>;
+    fn list(&self) -> Result<Response>;
+    fn download_file(&self, handle: &str) -> Result<Response>;
+    fn download_metadata(&self, handle: &str) -> Result<Response>;
+    fn delete(&self, handle: &str) -> Result<Response>;
+}
+
+impl ArtifactEndpoints for Client {
+    fn upload_file(&self, handle: &str, metadata: Metadata, filepath: &Path) -> Result<Response> {
         let file = fs::File::open(filepath)?;
 
         let form = multipart::Form::new()
@@ -27,50 +45,108 @@ impl Client {
             .text("metadata", serde_json::to_string(&metadata)?)
             .part("file", multipart::Part::reader(file));
 
+        let resp = self.client.put(self.url(A)).multipart(form).send()?;
+        Ok(resp)
+    }
+
+    fn list(&self) -> Result<Response> {
+        let resp = self.client.get(self.url(A)).send()?;
+
+        Ok(resp)
+    }
+
+    fn download_file(&self, handle: &str) -> Result<Response> {
         let resp = self
             .client
-            .put(self.url("/artifacts"))
-            .multipart(form)
+            .get(self.url(&format!("{A}/{handle}/file")))
             .send()?;
-
-        log::debug!("Received response: {resp:#?}");
         Ok(resp)
     }
 
-    pub fn list(&self) -> Result<Response> {
-        let resp = self.client.get(self.url("/artifacts")).send()?;
-        log::debug!("Received response: {:#?}", &resp);
-        Ok(resp)
-    }
-
-    pub fn download_file(&self, handle: &str) -> Result<Response> {
+    fn download_metadata(&self, handle: &str) -> Result<Response> {
         let resp = self
             .client
-            .get(self.url(&format!("/artifacts/{handle}/file")))
+            .get(self.url(&format!("{A}/{handle}/metadata")))
             .send()?;
-        log::debug!("Received response: {resp:#?}");
         Ok(resp)
     }
 
-    pub fn download_metadata(&self, handle: &str) -> Result<Response> {
+    fn delete(&self, handle: &str) -> Result<Response> {
         let resp = self
             .client
-            .get(self.url(&format!("/artifacts/{handle}/metadata")))
+            .delete(self.url(&format!("{A}/{handle}")))
             .send()?;
-        log::debug!("Received response: {resp:#?}");
         Ok(resp)
     }
+}
 
-    pub fn delete(&self, handle: &str) -> Result<Response> {
+pub trait RepositoryEndpoints {
+    fn register(&self, new_repository: &NewRepository) -> Result<Response>;
+    fn list(&self) -> Result<Response>;
+    fn show(&self, name: &str) -> Result<Response>;
+    fn credentials(&self, name: &str) -> Result<Response>;
+    fn delete(&self, name: &str) -> Result<Response>;
+}
+
+impl RepositoryEndpoints for Client {
+    fn register(&self, new_repository: &NewRepository) -> Result<Response> {
         let resp = self
             .client
-            .delete(self.url(&format!("/artifacts/{handle}")))
+            .put(self.url(R))
+            .body(serde_json::to_vec(new_repository)?)
             .send()?;
-        log::debug!("Received response: {resp:#?}");
         Ok(resp)
     }
 
-    fn url(&self, path: &str) -> String {
-        format!("{addr}{path}", addr = self.addr)
+    fn list(&self) -> Result<Response> {
+        let resp = self.client.get(self.url(R)).send()?;
+        Ok(resp)
+    }
+
+    fn show(&self, name: &str) -> Result<Response> {
+        let resp = self.client.get(self.url(&format!("{R}/{name}"))).send()?;
+        Ok(resp)
+    }
+
+    fn credentials(&self, name: &str) -> Result<Response> {
+        let resp = self
+            .client
+            .get(self.url(&format!("{R}/{name}/credentials")))
+            .send()?;
+        Ok(resp)
+    }
+
+    fn delete(&self, name: &str) -> Result<Response> {
+        let resp = self
+            .client
+            .delete(self.url(&format!("{R}/{name}")))
+            .send()?;
+        Ok(resp)
+    }
+}
+
+pub trait UserEndpoints {
+    fn create(&self) -> Result<Response>;
+    fn list(&self) -> Result<Response>;
+    fn revoke(&self, id: &str) -> Result<Response>;
+}
+
+impl UserEndpoints for Client {
+    fn create(&self) -> Result<Response> {
+        let resp = self.client.post(self.url(U)).send()?;
+        Ok(resp)
+    }
+
+    fn list(&self) -> Result<Response> {
+        let resp = self.client.get(self.url(U)).send()?;
+        Ok(resp)
+    }
+
+    fn revoke(&self, name: &str) -> Result<Response> {
+        let resp = self
+            .client
+            .delete(self.url(&format!("{U}/{name}")))
+            .send()?;
+        Ok(resp)
     }
 }
