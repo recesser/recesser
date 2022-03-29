ae7ee8511d261395b46aa966c594ecee94173e9e
nikstur <nikstur@outlook.com>
2022-03-29 22:19:02 +0200

    Readded verifying integrity of uploaded file and imrpoved error handling

--- apiserver/src/error.rs
+++ apiserver/src/error.rs
@@ -1,8 +1,12 @@
+use std::fmt::Debug;
+
 use actix_web::{http, HttpResponse, HttpResponseBuilder};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum UserError {
+    #[error("Advertised and computed handle of uploaded do not match.")]
+    Integrity,
     #[error("Request is not formatted properly.")]
     BadRequest,
     #[error("Access forbidden.")]
@@ -14,38 +18,48 @@ pub enum UserError {
 }
 
 impl UserError {
-    pub fn internal(e: anyhow::Error) -> Self {
-        log::debug!("{e:?}");
-        UserError::Internal
+    pub fn integrity(e: impl Debug) -> Self {
+        log_original_error(e);
+        UserError::Integrity
     }
 
-    pub fn unauthorized(e: anyhow::Error) -> Self {
-        log::debug!("{e:?}");
+    pub fn bad_request(e: impl Debug) -> Self {
+        log_original_error(e);
+        UserError::BadRequest
+    }
+
+    pub fn unauthorized(e: impl Debug) -> Self {
+        log_original_error(e);
         UserError::Unauthorized
     }
 
-    pub fn not_found(path: &str, e: impl std::error::Error) -> Self {
-        log::debug!("{e:?}");
+    pub fn not_found(path: &str, e: impl Debug) -> Self {
+        log_original_error(e);
         UserError::NotFound {
             path: path.to_string(),
         }
     }
 
-    pub fn bad_request(e: anyhow::Error) -> Self {
-        log::debug!("{e:?}");
-        UserError::BadRequest
+    pub fn internal(e: impl Debug) -> Self {
+        log_original_error(e);
+        UserError::Internal
     }
+}
+
+fn log_original_error(e: impl Debug) {
+    log::debug!("Original error: {e:?}")
 }
 
 impl actix_web::error::ResponseError for UserError {
     fn error_response(&self) -> HttpResponse {
-        log::debug!("{self:?}");
+        log::debug!("{self}");
         HttpResponseBuilder::new(self.status_code())
             .insert_header(http::header::ContentType::plaintext())
             .body(self.to_string())
     }
     fn status_code(&self) -> http::StatusCode {
         match *self {
+            UserError::Integrity => http::StatusCode::BAD_REQUEST,
             UserError::BadRequest => http::StatusCode::BAD_REQUEST,
             UserError::Unauthorized => http::StatusCode::UNAUTHORIZED,
             UserError::NotFound { .. } => http::StatusCode::NOT_FOUND,
--- apiserver/src/routes/artifact/upload.rs
+++ apiserver/src/routes/artifact/upload.rs
@@ -1,9 +1,11 @@
-use std::path::Path;
+use std::path::PathBuf;
+use std::str::FromStr;
 
 use actix_multipart::{Field, Multipart};
 use actix_web::{put, web, Error, HttpResponse};
 use anyhow::Result;
 use futures_util::TryStreamExt;
+use recesser_core::handle::Handle;
 use recesser_core::metadata::Metadata;
 use tokio::fs;
 use tokio::io::AsyncWriteExt;
@@ -18,7 +20,7 @@ async fn upload(
 ) -> Result<HttpResponse, Error> {
     let metadata_store = &app_state.database.metadata;
 
-    let mut handle: Option<String> = None;
+    let mut handle: Option<Handle> = None;
     let mut metadata: Option<Metadata> = None;
 
     while let Some(mut field) = payload.try_next().await? {
@@ -29,7 +31,7 @@ async fn upload(
 
         match field_name {
             "handle" => {
-                handle = extract_string(&mut field)
+                handle = extract_handle(&mut field)
                     .await
                     .map_err(UserError::bad_request)?;
             }
@@ -37,8 +39,9 @@ async fn upload(
                 let handle = handle.as_ref().ok_or(UserError::BadRequest)?;
                 log::debug!("Extracted handle: {handle}");
 
-                let buf = field.try_collect::<Vec<web::Bytes>>().await?.concat();
-                metadata = Some(serde_json::from_slice(&buf).map_err(|_| UserError::BadRequest)?);
+                metadata = extract_metadata(&mut field)
+                    .await
+                    .map_err(UserError::bad_request)?;
             }
             "file" => {
                 let handle = handle.as_ref().ok_or(UserError::BadRequest)?;
@@ -55,13 +58,11 @@ async fn upload(
                     log::debug!("File already exist in object storage. Skipping upload.");
                 } else {
                     log::debug!("File doesn't exist in object storage. Uploading it.");
-                    extract_and_upload_file(&mut field, metadata, &app_state)
-                        .await
-                        .map_err(UserError::internal)?
+                    extract_and_upload_file(&mut field, metadata, &app_state).await?
                 }
 
                 metadata_store
-                    .insert(handle, metadata)
+                    .insert(&handle.to_string(), metadata)
                     .await
                     .map_err(UserError::internal)?;
             }
@@ -71,31 +72,47 @@ async fn upload(
     Ok(HttpResponse::Ok().into())
 }
 
-async fn extract_string(field: &mut Field) -> Result<Option<String>> {
+async fn extract_handle(field: &mut Field) -> Result<Option<Handle>> {
     let buf = field.try_collect::<Vec<web::Bytes>>().await?.concat();
-    Ok(Some(String::from_utf8(buf)?))
+    let handle = Handle::from_str(&String::from_utf8(buf)?)?;
+    Ok(Some(handle))
+}
+
+async fn extract_metadata(field: &mut Field) -> Result<Option<Metadata>> {
+    let buf = field.try_collect::<Vec<web::Bytes>>().await?.concat();
+    Ok(Some(serde_json::from_slice(&buf)?))
 }
 
 async fn extract_and_upload_file(
     field: &mut Field,
     metadata: &Metadata,
     app_state: &web::Data<AppState>,
-) -> Result<()> {
-    let file = tempfile::NamedTempFile::new()?;
-    let filepath = file.path();
-    extract_file(field, filepath).await?;
+) -> std::result::Result<(), UserError> {
+    let file = tempfile::NamedTempFile::new().map_err(UserError::internal)?;
+    let file_path = file.into_temp_path();
+
+    let computed_object_handle = extract_file(field, file_path.to_path_buf())
+        .await
+        .map_err(UserError::bad_request)?;
+    computed_object_handle
+        .verify(&metadata.object_handle)
+        .map_err(UserError::integrity)?;
 
     app_state
         .objstore
-        .upload_file(&metadata.object_handle.to_string(), &filepath)
-        .await?;
+        .upload_file(computed_object_handle.to_string(), &file_path)
+        .await
+        .map_err(UserError::internal)?;
+
+    file_path.close().map_err(UserError::internal)?;
     Ok(())
 }
 
-async fn extract_file(field: &mut Field, file_path: &Path) -> Result<()> {
+async fn extract_file(field: &mut Field, file_path: PathBuf) -> Result<Handle> {
     let mut file = fs::File::create(&file_path).await?;
     while let Some(chunk) = field.try_next().await? {
         file.write_all(&chunk).await?;
     }
-    Ok(())
+    let handle = web::block(move || Handle::compute_from_file(&file_path)).await??;
+    Ok(handle)
 }
