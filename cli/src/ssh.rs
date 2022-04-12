0527004c59dbb6c144d8f6808f500f69845fe9dd
nikstur <nikstur@outlook.com>
2022-04-12 20:51:02 +0200

    Experimented with boringssl openssl dalek-ed25519

--- cli/src/ssh.rs
+++ cli/src/ssh.rs
@@ -87,3 +87,82 @@ fn clone_nth(buf: &str, position: usize) -> Result<String> {
     let split_content: Vec<&str> = buf.split_whitespace().collect();
     Ok(String::from(split_content[position]))
 }
+
+fn openssl_generate_key_pair() -> Result<String> {
+    let keypair = openssl::pkey::PKey::generate_ed25519()?;
+    let pem = keypair.private_key_to_pem_pkcs8()?;
+    Ok(String::from_utf8(pem)?)
+}
+
+use ed25519_dalek::Keypair;
+// use pkcs8::AlgorithmIdentifier;
+// use pkcs8::LineEnding;
+// use pkcs8::ObjectIdentifier;
+// use pkcs8::PrivateKeyInfo;
+use rand::rngs::OsRng;
+
+// See https://datatracker.ietf.org/doc/html/rfc8410#section-3
+// pub const Ed25519AlgorithmIdentifier: AlgorithmIdentifier = AlgorithmIdentifier {
+//     oid: ObjectIdentifier::new("1.3.101.112"),
+//     parameters: None,
+// };
+
+// fn dalek_generate_key_pair() -> Result<String> {
+//     let mut csprng = OsRng {};
+//     let keypair: Keypair = Keypair::generate(&mut csprng);
+//     let private_key_info = PrivateKeyInfo {
+//         algorithm: Ed25519AlgorithmIdentifier,
+//         private_key: keypair.secret.as_bytes(),
+//         public_key: None,
+//     };
+//     let pem = private_key_info.to_pem(LineEnding::default()).unwrap();
+//     Ok(pem.to_string())
+// }
+
+use ed25519::pkcs8::EncodePrivateKey;
+use ed25519::KeypairBytes;
+use pkcs8::LineEnding;
+
+fn dalek_generate_key_pair() -> Result<String> {
+    let mut csprng = OsRng {};
+    let keypair: Keypair = Keypair::generate(&mut csprng);
+    let keypair_bytes = KeypairBytes::from_bytes(&keypair.to_bytes());
+    let pem = keypair_bytes.to_pkcs8_pem(LineEnding::default()).unwrap();
+    Ok(pem.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Write;
+
+    use super::*;
+
+    // #[test]
+    // fn write_boringssl() -> Result<()> {
+    //     let key_pair = boring_generate_key_pair()?;
+    //     let pem = key_pair.private_key_to_pem_pkcs8()?;
+    //     println!("{:?}", String::from_utf8(pem.clone())?);
+
+    //     let encoded_pub_key = base64::encode(key_pair.public_key_to_der()?);
+    //     println!("{:?}", encoded_pub_key);
+
+    //     let mut file = std::fs::File::create("boring.pem")?;
+    //     file.write_all(&pem)?;
+
+    //     Ok(())
+    // }
+
+    #[test]
+    fn write_openssl() -> Result<()> {
+        let pem = openssl_generate_key_pair()?;
+        std::fs::write("openssl.pem", pem)?;
+        Ok(())
+    }
+
+    #[test]
+    fn write_dalek() -> Result<()> {
+        let pem = dalek_generate_key_pair()?;
+        std::fs::write("dalek2.pem", pem)?;
+        Ok(())
+    }
+}
