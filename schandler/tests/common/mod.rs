fa4968364372a2d6acc833c389925b9aa6606bdb
nikstur <nikstur@outlook.com>
2022-04-15 01:51:01 +0200

    Cleaned up function

--- schandler/tests/common/mod.rs
+++ schandler/tests/common/mod.rs
@@ -1,7 +1,9 @@
+use std::fs;
+use std::path::Path;
+
 use anyhow::Result;
 
 pub fn read_fixture(name: &str) -> Result<String> {
     let str_path = format!("tests/fixtures/{name}");
-    let s = std::fs::read_to_string(std::path::Path::new(&str_path))?;
-    Ok(s)
+    Ok(fs::read_to_string(Path::new(&str_path))?)
 }
