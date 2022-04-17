eafb9b35cc3c53905a53fe46af20763ee35c6997
nikstur <nikstur@outlook.com>
2022-04-18 00:20:50 +0200

    Added image tags and factored out building the image into a function

--- build-images.sh
+++ build-images.sh
@@ -6,7 +6,15 @@ binaries="
     schandler
 "
 
+build() {
+    binary=$1
+    docker build . \
+        --build-arg RUST_VERSION="${RUST_VERSION}" \
+        --build-arg BINARY="${binary}" \
+        --tag "recesser/${binary}:latest"
+}
+
 for b in ${binaries}; do
     echo "Building ${b}..."
-    docker build . --build-arg RUST_VERSION="${RUST_VERSION}" --build-arg BINARY="${b}"
+    build ${b}
 done
